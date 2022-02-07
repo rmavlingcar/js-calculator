b6be90693b2dc7e96e8b45e295ef93af1d87eeda
Ricky M <rickymavlingcar@live.com>
2022-02-07 10:48:02 -0600

    Added basic operations

---
+++ script.js
@@ -0,0 +1,19 @@
+// calculator app
+
+// add
+function add(x,y){
+    return (x + y)
+}
+// subtract
+function subtract(x,y) {
+    return (x - y)
+}
+// multiply
+function multiply(x,y) {
+    return (x * y)
+}
+// divide
+function divide(x,y){
+    return (a / b)
+}
+
